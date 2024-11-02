2d53fe27d0803bd09a84c9590bf4990445b40ff5
Caio Corsini <caiofilho14@gmail.com>
2024-11-02 02:05:50 -0300

    Primeiro commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/**
+ * PROJETO E ANALISE DE ALGORITMOS II
+ * TURMA 04P
+ * PROJETO BOMBEIROS
+ * ALAN MENIUK GLEIZER - 10416804
+ * CAIO VINICIUS CORSINI FILHO - 10342005
+ * GILBERTO DE MELO JÚNIOR - 10419275
+ * **/
+
+/**
+ * REFERENCIAS
+ * Algoritmo de Dijkstra Geeks for Geeks: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+ *
+ *
+ *  
+ *
+ * **/
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
